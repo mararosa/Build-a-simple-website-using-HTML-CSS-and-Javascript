2f55e6d2eaea206c5073edbc8af66ad21f0260dc
Mara Rosa <edimara.rosa@hotmail.com>
2020-01-05 19:21:07 -0300

    "Test moving console message."

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict'
-console.log('Here\'s a hidden message');
 
 let today = new Date()
 let formatDate = today.toDateString()
 let selectElement = document.getElementById('date')
 selectElement.innerHTML = formatDate
 
+console.log('Here\'s a hidden message');
 // console.log('This code is no longer active')
